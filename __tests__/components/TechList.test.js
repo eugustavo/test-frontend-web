26a295366d879736429fadced3f5433c488dc838
eugustavo <gustavo.ifto@gmail.com>
2020-06-11 22:40:41 -0300

    🃏 Test: Add new tech and New tech in storage

--- __tests__/components/TechList.test.js
+++ __tests__/components/TechList.test.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import TechList from '~/components/TechList';
 
 describe('TechList Component', () => {
+  afterEach(async () => await cleanup());
+  beforeEach(() => localStorage.clear());
 
   it('should be able to add new tech', () => {
-    const { getByText, getByTestId } = render(<TechList />);
+    const { getByText, getByTestId, getByLabelText } = render(<TechList />);
 
-    fireEvent.click(getByText('Adicionar'));
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+    fireEvent.submit(getByTestId('tech-form'));
 
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    expect(getByLabelText('Tech')).toHaveValue('');
   })
-  
+
+  it('should store techs in storage', async () => {
+    let { getByTestId, getByLabelText, getByText } = render(<TechList />);
+
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+    fireEvent.submit(getByTestId('tech-form'))
+
+    await cleanup();
+    ({ getByTestId, getByLabelText, getByText } = render(<TechList />));
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']))
+    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+  });
+
 });
