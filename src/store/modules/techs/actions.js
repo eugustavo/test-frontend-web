ea37ff1efc055479c10fd68fc6d54fec8c60e5ba
eugustavo <gustavo.ifto@gmail.com>
2020-06-15 20:50:55 -0300

    🃏 Finished Tests

---
+++ src/store/modules/techs/actions.js
@@ -0,0 +1,19 @@
+export function addTech(tech) {
+  return {
+    type: '@techs/ADD_TECH',
+    payload: { tech }
+  }
+}
+
+export function getTechsSuccess(data) {
+  return {
+    type: '@techs/GET_TECHS_SUCCESS',
+    payload: { data }
+  }
+}
+
+export function getTechsFailure() {
+  return {
+    type: '@techs/GET_TECHS_FAILURE',
+  }
+}
